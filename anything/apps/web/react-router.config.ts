3622406b9e9e640a5a2932f40ee1f651307a88f8
Mark Mugisha <141035540+markmugisha@users.noreply.github.com>
2026-04-12 20:02:07 +0300

    Revert react-router config

--- anything/apps/web/react-router.config.ts
+++ anything/apps/web/react-router.config.ts
@@ -3,8 +3,4 @@ import type { Config } from '@react-router/dev/config';
 export default {
   appDirectory: './src/app',
   ssr: true,
-  serverBundles({ branch }) {
-    // All routes use a single server bundle
-    return 'server';
-  },
 } satisfies Config;
